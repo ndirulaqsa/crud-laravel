c7d7f38fd82d98c3f0f3dde9214b8b06277e4f50
andikhairulaqsa <andikhairulaqsa@gmail.com>
2026-06-16 19:21:51 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\ItemController;
 
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('items', ItemController::class);
+Route::put('/items/{id}', [ItemController::class, 'update']);
+Route::delete('/items/{id}', [ItemController::class, 'destroy']);
 
 // Tambahkan ini untuk fitur markAsFound
 Route::put('/items/{id}/found', [ItemController::class, 'markAsFound']);
