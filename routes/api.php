b00e6d9ca3895c2224fbf96c303cae07912e9e0f
andikhairulaqsa <andikhairulaqsa@gmail.com>
2026-06-28 19:50:45 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -12,13 +12,13 @@ Route::post('/login', [AuthController::class, 'login']);
 // === ITEMS (public) ===
 Route::get('/items', [ItemController::class, 'index']);
 Route::get('/items/{id}', [ItemController::class, 'show']);
+Route::post('/items', [ItemController::class, 'store']);
 
 // === PROTECTED (butuh login) ===
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
 
-    Route::post('/items', [ItemController::class, 'store']);
     Route::put('/items/{id}', [ItemController::class, 'update']);
     Route::delete('/items/{id}', [ItemController::class, 'destroy']);
     Route::put('/items/{id}/found', [ItemController::class, 'markAsFound']);
