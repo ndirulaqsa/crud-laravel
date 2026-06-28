af6e89ef6892de02231e60be24bc0892cf97de8f
andikhairulaqsa <andikhairulaqsa@gmail.com>
2026-06-28 19:00:33 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,10 +17,12 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
+   protected $fillable = [
+    'name',
+    'phone',
+    'role',
+    'email',
+    'password',
     ];
 
     /**
